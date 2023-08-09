024401cce3b96ade86175bc14bd3f74d7cf7893e
egorgavriloff2008 <egornicolaevich2008@gmail.com>
2023-08-09 22:00:57 +0300

    initial

---
+++ app_advertisements/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import index, top_sellers
+
+urlpatterns = [
+    path ('', index, name ='/'),
+    path ('top-sellers/', top_sellers, name = 'top-sellers'),
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app_advertisements/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Advertisement
+def index(request):
+    advertisements = Advertisement.objects.all
+    contex = {'advertisements':advertisements}
+    return render(request, 'index.html', contex)
+
+def top_sellers(request):
+   return render(request, 'top-sellers.html')
+
+def register(request):
+
+
+
+
+
+
+
